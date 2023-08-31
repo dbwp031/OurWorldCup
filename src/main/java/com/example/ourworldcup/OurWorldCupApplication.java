6ab5c13ab759f05b98f3c6cefa7e06a324a79295
Yuje Lee <dbwp031@gmail.com>
2023-09-01 08:55:14 +0900

    프로젝트 생성
    프로젝트를 생성했다. 기본 개발을 위한 dependency만 추가했다.
    `spring security`와 같은 부분은 이후 개발 중 필요할 때 추가하도록 한다.

---
+++ src/main/java/com/example/ourworldcup/OurWorldCupApplication.java
@@ -0,0 +1,13 @@
+package com.example.ourworldcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurWorldCupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurWorldCupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ourworldcup/OurWorldCupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ourworldcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurWorldCupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
