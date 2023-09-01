c66647ad869d86821683c99d15e94be70ca2c0d1
Yuje Lee <dbwp031@gmail.com>
2023-09-01 10:52:03 +0900

    feat #4 - AuditingFields 추가
    중복되는 필드인 `createdAt`, `createdBy`, `modifiedAt`, `modifiedBy`를 @MappedSuperClass를 사용해서 추상클래스로 만들어 엔티티들이 확장할 수 있도록 하였다.
    현재 auditor를 알기위한 코드를 JpaConfig에서 작성해주어야 하는데, 이 부분은 이후 Spring Security 설정을 할 때 자세히 하기로 함.  현재는 현재 수정자, 작성자 등의 정보를 얻는 방법이 없기 때문이다.

---
+++ src/main/java/com/example/ourworldcup/config/JpaConfig.java
@@ -0,0 +1,15 @@
+package com.example.ourworldcup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("수정자"); //TODO: 현재는 지금 수정중인 유저의 정보를 알 수 있는 방법이 없다. 이후 Spring Security로 보안 설정시 가동할 수 있도록 할 것.
+    }
+}
