e01e265ed87e12fd0949d896cd9cd07380b21564
Yuje Lee <dbwp031@gmail.com>
2023-09-16 01:51:11 +0900

    #4  - WorldcupRestController deleteItem 수정 및 ajax가 필요로하는 response값 넣어줌

--- src/main/java/com/example/ourworldcup/controller/worldcup/WorldcupRestController.java
+++ src/main/java/com/example/ourworldcup/controller/worldcup/WorldcupRestController.java
@@ -1,15 +1,20 @@
 package com.example.ourworldcup.controller.worldcup;
 
 import com.example.ourworldcup.domain.Worldcup;
+import com.example.ourworldcup.service.item.ItemService;
 import com.example.ourworldcup.service.worldcup.WorldcupService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/worldcup")
@@ -18,13 +23,16 @@ public class WorldcupRestController {
     private static final String SESSION_ATTR_WORLDCUP = "worldcup";
     private static final String SESSION_ATTR_ITEMS = "items";
     private final WorldcupService worldcupService;
+    private final ItemService itemService;
     @DeleteMapping("/item/{id}")
-    public void deleteItem(@PathVariable Long id, HttpSession session, HttpServletResponse response) {
-        response.setContentType("application/json");
-        response.setCharacterEncoding("utf-8");
+    public ResponseEntity<Map<String, Boolean>> deleteItem(@PathVariable Long id, HttpSession session, HttpServletResponse response) {
         Worldcup worldcup = (Worldcup) session.getAttribute(SESSION_ATTR_WORLDCUP);
 
-        worldcupService.deleteItem(worldcup.getId(), id);
-        response.setStatus(HttpStatus.OK.value());
+        itemService.deleteItem(id);
+
+        Map<String, Boolean> responseMap = new HashMap<>();
+        responseMap.put("success", true);
+
+        return new ResponseEntity<>(responseMap, HttpStatus.OK);
     }
 }
