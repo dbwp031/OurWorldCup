7e8e6bbe6d08a0245f7ef1159ebeb6789c518b54
Yuje Lee <dbwp031@gmail.com>
2023-10-09 11:37:02 +0900

    #50 feat: @AuthUser 설정
    ParameterResolver @AuthUser를 추가한다. WebConfig파일도 설정에 필요하다. 이번엔 이전 커밋에 해당 파일이 포함되어 있다.

---
+++ src/main/java/com/example/ourworldcup/auth/AuthUserArgumentResolver.java
@@ -0,0 +1,45 @@
+package com.example.ourworldcup.auth;
+
+import com.example.ourworldcup.auth.dto.UserAccountDto;
+import com.example.ourworldcup.converter.userAccount.UserAccountConverter;
+import com.example.ourworldcup.domain.userAccount.UserAccount;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        AuthUser authUser = parameter.getParameterAnnotation(AuthUser.class);
+        if (authUser == null) return false;
+        if (!parameter.getParameterType().equals(UserAccount.class)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Object principal = null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            System.out.println("error: 1");
+            principal = authentication.getPrincipal();
+        }
+        if (principal == null || principal.getClass() == String.class) {
+            System.out.println("error: 2");
+            return null;
+        }
+        UserAccountDto loginUser = (UserAccountDto) principal;
+        return UserAccountConverter.toUserAccount(loginUser);
+    }
+}
